af40246be04827fd9d6e00fa2968add3341aec24
brokoli <bocalist@gmail.com>
2024-09-09 14:28:26 -0600

    using variables

--- app/index.js
+++ app/index.js
@@ -1,23 +1,30 @@
-const { View, Text } = require("react-native")
+const { View, Text } = require("react-native");
 
 const Index = () => {
-    let name = 'Hyunseung';
-    const age = 40;
+  let name = "Hyunseung";
+  name = "Ted";
+  const age = 40;
 
-    return (
-        <View style={{
-            flex: 1,
-            justifyContent: 'center',
-            alignItems: 'center',
-            backgroundColor: 'lightblue',
-        }}>
-            <Text style={{
-                fontSize: 24,
-                fontWeight: 'bold',
-                color: 'darkblue',
-            }}>Hello, {name}! {age} years old.</Text>
-        </View>
-    );
-}
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "lightblue",
+      }}
+    >
+      <Text
+        style={{
+          fontSize: 24,
+          fontWeight: "bold",
+          color: "darkblue",
+        }}
+      >
+        Hello, {name}! {age} years old.
+      </Text>
+    </View>
+  );
+};
 
 export default Index;
