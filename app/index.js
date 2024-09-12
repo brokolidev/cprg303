f548ed22efcaa33c1b4aa72152449f7114316f8c
Hyunseung Choi <bocalist@gmail.com>
2024-09-12 15:17:43 -0600

    adding buttons and pressable

--- app/index.js
+++ app/index.js
@@ -1,30 +1,33 @@
-const { View, Text } = require("react-native");
+import React, {Component} from 'react';
+import {Button, Pressable, Text, View} from 'react-native';
 
 const Index = () => {
-  let name = "Hyunseung";
-  name = "Ted";
-  const age = 40;
+    const handleTouch = () => {
+        alert("Button Clicked");
+    };
 
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "lightblue",
-      }}
-    >
-      <Text
-        style={{
-          fontSize: 24,
-          fontWeight: "bold",
-          color: "darkblue",
-        }}
-      >
-        Hello, {name}! {age} years old.
-      </Text>
-    </View>
-  );
-};
+    return (
+        <View style={{ flex: 0.5 }}>
+            <View style={{flex: 1, backgroundColor: "red"}}></View>
+            <View style={{flex: 2, backgroundColor: "green"}}></View>
+            <View style={{flex: 3, backgroundColor: "blue"}}></View>
+            <Text style={{fontSize: 40}}>Hello World</Text>
+
+            <View style={{ margin: 10 }}>
+                <Button title="Click Me" color="orange" onPress={handleTouch}></Button>
+            </View>
+            <View style={{ margin: 20 }}>
+                <Button title="Click Me" color="blue" onPress={handleTouch}></Button>
+            </View>
+            <View style={{ margin: 30 }}>
+                <Button title="Click Me" color="green" onPress={handleTouch}></Button>
+            </View>
+
+            <Pressable onPress={handleTouch}>
+                <Text style={{fontSize: 40}} onPress={handleTouch}>Pressable Button</Text>
+            </Pressable>
+        </View>
+    );
+}
 
 export default Index;
