43129c8b2d80846af457dffd6bf073c7ac2764bd
brokolidev <bocalist@gmail.com>
2024-09-16 13:43:50 -0600

    creating a child component

--- app/index.js
+++ app/index.js
@@ -1,33 +1,23 @@
-import React, {Component} from 'react';
-import {Button, Pressable, Text, View} from 'react-native';
+import {Text, View} from "react-native";
 
 const Index = () => {
-    const handleTouch = () => {
-        alert("Button Clicked");
-    };
-
-    return (
-        <View style={{ flex: 0.5 }}>
-            <View style={{flex: 1, backgroundColor: "red"}}></View>
-            <View style={{flex: 2, backgroundColor: "green"}}></View>
-            <View style={{flex: 3, backgroundColor: "blue"}}></View>
-            <Text style={{fontSize: 40}}>Hello World</Text>
-
-            <View style={{ margin: 10 }}>
-                <Button title="Click Me" color="orange" onPress={handleTouch}></Button>
-            </View>
-            <View style={{ margin: 20 }}>
-                <Button title="Click Me" color="blue" onPress={handleTouch}></Button>
-            </View>
-            <View style={{ margin: 30 }}>
-                <Button title="Click Me" color="green" onPress={handleTouch}></Button>
-            </View>
-
-            <Pressable onPress={handleTouch}>
-                <Text style={{fontSize: 40}} onPress={handleTouch}>Pressable Button</Text>
-            </Pressable>
-        </View>
-    );
-}
+	return (
+		<View style={{flex: 1, justifyContent: "center", alignItems: "center"}}>
+			<Text style={{fontSize: 25, backgroundColor: "yellow"}}>
+				This is a parent component
+			</Text>
+			<Custom_text/>
+		</View>
+	);
+};
 
 export default Index;
+
+const Custom_text = () => {
+	return (
+		<View>
+			<Text style={{fontSize: 25, backgroundColor: "green"}}>This is a child component</Text>
+		</View>
+	);
+};
+
