19e16e41595fb49440c9f4a0e23f05596a6819f5
brokolidev <bocalist@gmail.com>
2024-12-11 18:52:01 -0700

    added tab layout

--- app/_layout.js
+++ app/_layout.js
@@ -1,2 +1,10 @@
-// Import your global CSS file
 import "../global.css";
+import {Stack} from 'expo-router/stack';
+
+export default function Layout() {
+	return (
+		<Stack>
+			<Stack.Screen name="(tabs)" options={{headerShown: false}}/>
+		</Stack>
+	);
+}
